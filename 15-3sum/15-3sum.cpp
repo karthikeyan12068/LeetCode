c99a957a86ca70a3fb7fd09e7fcfefdd32fdb164
Karthikeyan <76770213+karthikeyan12068@users.noreply.github.com>
2022-08-02 13:26:47 +0530

    Time: 165 ms (44.61%), Space: 28.2 MB (15.28%) - LeetHub

---
+++ 15-3sum/15-3sum.cpp
@@ -0,0 +1,121 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long  ll;
+typedef vector<int> v_i;
+typedef vector<long long int> v_ll;
+typedef vector<pair<int,int>> vp_i;
+typedef vector<pair<long long int,long long int>> vp_ll;
+typedef map<int,int> m_i;
+typedef map<long long int,long long int> m_ll;
+typedef map<pair<int,int>,int> mp_i;
+typedef map<pair<long long int,long long int>,long long int> mp_ll;
+typedef set<int> s_i;
+typedef set<long long int> s_ll;
+typedef set<pair<int,int>> sp_i;
+typedef set<pair<long long int,long long int>> sp_ll;
+#define mp make_pair
+#define fi first
+#define se second
+#define INF 2e18
+#define sorts(v) sort((v).begin(),(v).end())
+#define rev(v) reverse((v).begin(),(v).end())
+#define count2(v,s) count((v).begin(),(v).end(),(s))
+#define min3(a,b,c)     min(a,min(b,c))
+#define max3(a,b,c)     max(a,max(b,c))
+#define min4(a,b,c,d)   min(min(a,b),min(c,d))
+#define max4(a,b,c,d)   max(max(a,b),max(c,d))
+#define max5(a,b,c,d,e) max(max3(a,b,c),max(d,e))
+#define min5(a,b,c,d,e) min(min3(a,b,c),min(d,e))
+#define forn(a,b,c) for(ll (c)=(a);(c)<(b);(c)++)
+#define forr(a,b,c) for(ll (c)=(a);(i)>=(b);(c)--)
+#define rep1(s) for(auto &it:(s))
+#define rep2(a,b,c) for(ll i=(a);i<(b);i+=(c))
+ll binets_formula(ll n) {
+    double sqrt5 = sqrt(5);
+    ll F_n = (pow((1 + sqrt5), n) - pow((1 - sqrt5), n)) / (pow(2, n) * sqrt5);
+    return F_n;
+}
+bool sortbysec(const pair<int,int> &a,const pair<int,int> &b)
+{
+    return (a.second < b.second);
+}
+string to_binary(ll val)
+{
+    return (std::bitset<30>(val).to_string());
+}
+ll to_decimal(const string& s)
+{
+    return (std::bitset<30>(s).to_ulong());
+}
+double log_a_to_base_b(double a, double b)
+{
+    return log(a) / log(b);
+}
+ll str_to_in(string s)
+{
+    stringstream geek(s);
+    ll x = 0ll;
+    geek >> x;
+    return x;
+}
+
+ll sum_v(v_ll v1){
+    ll sum=0;
+    forn(0,v1.size(),i){
+        sum+=v1[i];
+    }
+    return sum;
+}
+ll ceil(ll x, ll y) {
+    ll ans = x % y == 0 ? x / y : x / y + 1;
+    return ans;
+}
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sorts(nums);
+        
+        ll n=nums.size();
+        
+        map<vector<int>,ll>m2;
+        vector<vector<int>>v1;
+        forn(0,n-2,i){
+            if(nums[i]>0){
+                return v1;
+            }
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
+            }
+            
+            ll left=i+1,right=n-1;
+            while(left<right){
+                ll sum=nums[i]+nums[left]+nums[right];
+                if(sum<0){
+                    left++;
+                }
+                else{
+                    if(sum>0){
+                        right--;
+                    }
+                    else{
+                        vector<int>v;
+                        v.push_back(nums[i]);
+                        v.push_back(nums[left]);
+                        v.push_back(nums[right]);
+                        if(m2[v]==0){
+                            m2[v]=1;
+                            v1.push_back(v);
+                        }
+                        left++;
+                        while(nums[left]==nums[left-1] && left<right){
+                            left++;
+                        }
+                    }
+                }
+            }
+        }
+        return v1;
+    }
+    
+};

7a901c1796c1a3da1e97d2f167c16c56b53bb6f6
Karthikeyan <76770213+karthikeyan12068@users.noreply.github.com>
2022-08-02 19:10:41 +0530

    Time: 7 ms (58.86%), Space: 10.7 MB (74.80%) - LeetHub

---
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -0,0 +1,138 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long  ll;
+typedef vector<int> v_i;
+typedef vector<long long int> v_ll;
+typedef vector<pair<int,int>> vp_i;
+typedef vector<pair<long long int,long long int>> vp_ll;
+typedef map<int,int> m_i;
+typedef map<long long int,long long int> m_ll;
+typedef map<pair<int,int>,int> mp_i;
+typedef map<pair<long long int,long long int>,long long int> mp_ll;
+typedef set<int> s_i;
+typedef set<long long int> s_ll;
+typedef set<pair<int,int>> sp_i;
+typedef set<pair<long long int,long long int>> sp_ll;
+#define mp make_pair
+#define fi first
+#define se second
+#define INF 2e18
+#define sorts(v) sort((v).begin(),(v).end())
+#define rev(v) reverse((v).begin(),(v).end())
+#define count2(v,s) count((v).begin(),(v).end(),(s))
+#define min3(a,b,c)     min(a,min(b,c))
+#define max3(a,b,c)     max(a,max(b,c))
+#define min4(a,b,c,d)   min(min(a,b),min(c,d))
+#define max4(a,b,c,d)   max(max(a,b),max(c,d))
+#define max5(a,b,c,d,e) max(max3(a,b,c),max(d,e))
+#define min5(a,b,c,d,e) min(min3(a,b,c),min(d,e))
+#define forn(a,b,c) for(ll (c)=(a);(c)<(b);(c)++)
+#define forr(a,b,c) for(ll (c)=(a);(i)>=(b);(c)--)
+#define rep1(s) for(auto &it:(s))
+#define rep2(a,b,c) for(ll i=(a);i<(b);i+=(c))
+ll binets_formula(ll n) {
+    double sqrt5 = sqrt(5);
+    ll F_n = (pow((1 + sqrt5), n) - pow((1 - sqrt5), n)) / (pow(2, n) * sqrt5);
+    return F_n;
+}
+bool sortbysec(const pair<int,int> &a,const pair<int,int> &b)
+{
+    return (a.second < b.second);
+}
+string to_binary(ll val)
+{
+    return (std::bitset<30>(val).to_string());
+}
+ll to_decimal(const string& s)
+{
+    return (std::bitset<30>(s).to_ulong());
+}
+double log_a_to_base_b(double a, double b)
+{
+    return log(a) / log(b);
+}
+string str(char c){
+    string s;
+    stringstream ss;
+    ss << c;
+    ss >> s;                
+    return s;
+}
+template<typename T>
+void pop_front(std::vector<T> &v)
+{
+    if (v.size() > 0) {
+        v.erase(v.begin());
+    }
+}
+ll str_to_in(string s)
+{
+    stringstream geek(s);
+    ll x = 0ll;
+    geek >> x;
+    return x;
+}
+
+ll sum_v(v_ll v1){
+    ll sum=0;
+    forn(0,v1.size(),i){
+        sum+=v1[i];
+    }
+    return sum;
+}
+ll ceil(ll x, ll y) {
+    ll ans = x % y == 0 ? x / y : x / y + 1;
+    return ans;
+}
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* t1=head,*t2=head;
+        ll c=0;
+        while(t1!=NULL){
+            t1=t1->next;
+            c++;
+        }
+        if(c==1 && n==1){
+            head=NULL;
+            return head;
+        }
+        t1=t2;
+        if(c==n){
+            
+            head=head->next;
+            delete t1;
+            return head;
+        }
+        if(n==1){
+            while(t1->next!=NULL){
+                t2=t1;
+                t1=t1->next;
+            }
+            t2->next=NULL;
+            delete t1;
+            return head;
+            
+        }
+        c=c-n;
+        while(c--){
+            t2=t1;
+            t1=t1->next;
+        }
+        t2->next=t1->next;
+        delete(t1);
+        
+        return head;
+    }
+};

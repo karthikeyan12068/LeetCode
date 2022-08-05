ccfc34c8d55cb620a88ed6fb53f4cc62d3b88612
Karthikeyan <76770213+karthikeyan12068@users.noreply.github.com>
2022-08-06 00:18:45 +0530

    Time: 181 ms (5.15%), Space: 26 MB (6.45%) - LeetHub

---
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -0,0 +1,156 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long  ll;
+typedef vector<int> v_i;
+typedef vector<long long int> v_ll;
+typedef vector<pair<int,int>> vp_i;
+typedef vector<pair<long long int,long long int>> vp_ll;
+typedef map<int,int> m_i;
+typedef map<long long int,long long int> m_ll;
+typedef map<pair<int,int>,int> mp_i;
+typedef map<pair<long long int,long long int>,long long int> mp_ll;
+typedef set<int> s_i;
+typedef set<long long int> s_ll;
+typedef set<pair<int,int>> sp_i;
+typedef set<pair<long long int,long long int>> sp_ll;
+#define mp make_pair
+#define fi first
+#define se second
+#define INF 2e18
+#define sorts(v) sort((v).begin(),(v).end())
+#define rev(v) reverse((v).begin(),(v).end())
+#define count2(v,s) count((v).begin(),(v).end(),(s))
+#define min3(a,b,c)     min(a,min(b,c))
+#define max3(a,b,c)     max(a,max(b,c))
+#define min4(a,b,c,d)   min(min(a,b),min(c,d))
+#define max4(a,b,c,d)   max(max(a,b),max(c,d))
+#define max5(a,b,c,d,e) max(max3(a,b,c),max(d,e))
+#define min5(a,b,c,d,e) min(min3(a,b,c),min(d,e))
+#define forn(a,b,c) for(ll (c)=(a);(c)<(b);(c)++)
+#define forr(a,b,c) for(ll (c)=(a);(i)>=(b);(c)--)
+#define rep1(s) for(auto &it:(s))
+#define rep2(a,b,c) for(ll i=(a);i<(b);i+=(c))
+ll binets_formula(ll n) {
+    double sqrt5 = sqrt(5);
+    ll F_n = (pow((1 + sqrt5), n) - pow((1 - sqrt5), n)) / (pow(2, n) * sqrt5);
+    return F_n;
+}
+bool sortbysec(const pair<int,int> &a,const pair<int,int> &b)
+{
+    return (a.second < b.second);
+}
+string to_binary(ll val)
+{
+    return (std::bitset<30>(val).to_string());
+}
+ll to_decimal(const string& s)
+{
+    return (std::bitset<30>(s).to_ulong());
+}
+double log_a_to_base_b(double a, double b)
+{
+    return log(a) / log(b);
+}
+string str(char c){
+    string s;
+    stringstream ss;
+    ss << c;
+    ss >> s;
+    return s;
+}
+template<typename T>
+void pop_front(std::vector<T> &v)
+{
+    if (v.size() > 0) {
+        v.erase(v.begin());
+    }
+}
+ll str_to_in(string s)
+{
+    stringstream geek(s);
+    ll x = 0ll;
+    geek >> x;
+    return x;
+}
+
+ll sum_v(v_ll v1){
+    ll sum=0;
+    forn(0,v1.size(),i){
+        sum+=v1[i];
+    }
+    return sum;
+}
+ll ceil(ll x, ll y) {
+    ll ans = x % y == 0 ? x / y : x / y + 1;
+    return ans;
+}
+TreeNode* insert(TreeNode* root,int data){
+    TreeNode* node=new TreeNode(data);
+    if(root==NULL){
+        root=node;
+    }
+    else{
+        TreeNode* cur=root;
+        TreeNode* temp=cur;
+        while(cur!=NULL){
+            if(data>cur->val){
+                temp=cur;
+                cur=cur->right;
+            }
+            else{
+                temp=cur;
+                cur=cur->left;
+            }
+        }
+        if(temp->val<data){
+            temp->right=node;
+        }
+        else{
+            temp->left=node;
+        }
+    }
+    return root;
+}
+void preorder(TreeNode* root,v_ll &v1){
+    if(root!=NULL){
+        preorder(root->left,v1);
+        v1.push_back(root->val);
+        preorder(root->right,v1);
+    }
+}
+
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        TreeNode* root1=NULL;
+        v_ll v1;
+        preorder(root,v1);
+        forn(0,v1.size(),i){
+            root1=insert(root1,v1[i]);
+        }
+        m_ll m1;
+        rep1(v1){
+            m1[it]++;
+            if(m1[it]>1){
+                return false;
+            }
+        }
+        v_ll v2;
+        preorder(root1,v2);
+        if(v1==v2){
+            return true;
+        }
+        return false;
+    }
+};

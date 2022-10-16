cc1794f461c2c9071d7847cc6b342f2ed98419c1
Karthikeyan <76770213+karthikeyan12068@users.noreply.github.com>
2022-10-16 07:19:57 +0530

    Time: 14 ms (5.82%), Space: 7 MB (8.34%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -1,9 +1,30 @@
 class Solution {
 public:
-    bool divisorGame(int n) {
-        if(n%2==0){
-            return true;
+    int help(int n,map<int,int>&m){
+        if(n<=1){
+            m[n]=-1;
+            return 0;
         }
-        return false;
+        if(m[n]==-1){
+            return 0;
+        }
+        if(m[n]==1){
+            return 1;
+        }
+        for(int i=1;i<n;i++){
+            if(n%i==0){
+                if(help(n-i,m)==0){
+                    m[n]=1;
+                    return 1;
+                }
+            }
+        }
+        m[n]=-1;
+        return 0;
+        
+    }
+    bool divisorGame(int n) {
+        map<int,int>m1;
+        return help(n,m1);
     }
 };

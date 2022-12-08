94577d0cb4b56466126d04cd172101773f417ef8
Karthikeyan <76770213+karthikeyan12068@users.noreply.github.com>
2022-12-08 23:39:06 +0530

    Time: 13 ms (51.27%), Space: 9 MB (52.75%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int check(vector<vector<int>>& nums,int r,int c,vector<vector<int>>&dp)
+    {
+          if(r==nums.size()||c>nums[r].size())
+            return 0;
+        
+        if(dp[r][c]!=-1)
+            return dp[r][c];
+        
+     return dp[r][c]=nums[r][c]+min(check(nums,r+1,c,dp),check(nums,r+1,c+1,dp));
+        
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int row=triangle.size();
+        vector<vector<int>>dp(row,vector<int>(row,-1));
+        int sol=INT_MAX;
+        sol=min(sol,check(triangle,0,0,dp)) ;
+     return sol;
+    }
+};

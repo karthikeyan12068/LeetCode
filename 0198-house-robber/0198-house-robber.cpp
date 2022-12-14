b2ba343d4214b4f151e0e24eeb2879ddfcf9c346
Karthikeyan <76770213+karthikeyan12068@users.noreply.github.com>
2022-12-14 08:08:21 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (25.79%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int ret(vector<int>&ans,int sum,int i,int &max1,vector<int>&dp){
-        if(i>=ans.size()){
+        if(i<0){
             max1=max(max1,sum);
             return 0;
         }
@@ -9,14 +9,14 @@ public:
             max1=max(max1,sum+dp[i]);
             return dp[i];
         }
-        int inc=ans[i]+ret(ans,sum+ans[i],i+2,max1,dp);
-        int ninc=0+ret(ans,sum+0,i+1,max1,dp);
+        int inc=ans[i]+ret(ans,sum+ans[i],i-2,max1,dp);
+        int ninc=0+ret(ans,sum+0,i-1,max1,dp);
         return dp[i]=max(inc,ninc);
     }
     int rob(vector<int>& nums) {
         int i=0,max1=-1,sum=0;
         vector<int>dp(nums.size(),-1);
-        ret(nums,sum,i,max1,dp);
+        ret(nums,sum,nums.size()-1,max1,dp);
         return max1;
     }
 };

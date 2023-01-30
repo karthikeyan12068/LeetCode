23da0331de49184a1395f51c8ecf8b297ef82e7b
Karthikeyan <76770213+karthikeyan12068@users.noreply.github.com>
2023-01-31 01:22:21 +0530

    Time: 626 ms (50.36%), Space: 205.6 MB (65.14%) - LeetHub

--- 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
-  vector<int> fun(vector<vector<int>> &adj, string &labels, int i,vector<int>&result){
-    vector<int> ans(26, 0);
-    result[i] = 1;
-    ans[labels[i] - 'a'] = 1;
-    
-    for(int j = 0; j != adj[i].size(); j++)
-      if(!result[adj[i][j]]){
-        vector<int> tmp = fun(adj, labels,adj[i][j],result);
-        for(int k = 0; k != 26; k++) ans[k] += tmp[k];
-      }
-    
-    result[i] = ans[labels[i] - 'a'];
-    
-    return ans;
-  }
-  
-  vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labels) {
-    vector<vector<int>> adj(n);
-    vector<int> result(n,0);
-    for(int i = 0; i != edges.size(); i++)
-     {adj[edges[i][0]].push_back(edges[i][1]);
-      adj[edges[i][1]].push_back(edges[i][0]);
-     }
-    
-    fun(adj, labels, 0,result);
-    return result;
-  }
+    vector<int> ret(int i,string &labels,vector<vector<int>>&adj,vector<int>&ans){
+        vector<int>temp(26,0);
+        temp[labels[i]-'a']=1;
+        ans[i]=1;
+        for(int j=0;j<adj[i].size();j++){
+            if(!ans[adj[i][j]]){
+                vector<int>c=ret(adj[i][j],labels,adj,ans);
+                for(int k=0;k<26;k++){
+                    temp[k]+=c[k];
+                }
+            }
+        }
+        ans[i]=temp[labels[i]-'a'];
+        return temp;
+    }
+    vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labels) {
+        vector<vector<int>>adj(n);
+        for(int i=0;i<edges.size();i++){
+            adj[edges[i][0]].push_back(edges[i][1]);
+            adj[edges[i][1]].push_back(edges[i][0]);
+        }
+        vector<int>ans(n,0);
+        ret(0,labels,adj,ans);
+        return ans;
+    }
 };

6519784fe2c7fc6150f4bbe77d6ccd3941226367
Karthikeyan <76770213+karthikeyan12068@users.noreply.github.com>
2023-02-11 10:31:59 +0530

    Time: 56 ms (96.05%), Space: 19.8 MB (69.88%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -1,35 +1,41 @@
 class Solution {
 public:
+    
     int maxDistance(vector<vector<int>>& grid) {
-        int n = grid.size();
-        queue<pair<int, int>> q;
-        //Note: The copy matrix visited can be avoided by updating the original matrix grid but changing the input parameter is not considered under good practices. That's why we are taking extra 2d matrix visited
-        vector<vector<int>> visited = grid;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (visited[i][j] == 1) 
-                    q.push({i, j});
+        int vis[grid.size()][grid.size()];
+        vector<pair<int,int>>dir={{0,1},{1,0},{0,-1},{-1,0}};
+        queue<pair<int,int>>q;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid.size();j++){
+                if(grid[i][j]==1){
+                    vis[i][j]=1;
+                    q.push({i,j});
+                }
+                else{
+                    vis[i][j]=0;
+                }
             }
         }
-        if (q.empty() || q.size() == n * n) 
-            return -1;
-        int distance = -1;
-        // visit all four direction i.e. right, left, up, down
-        vector<pair<int, int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        while (!q.empty()) {
-            int size = q.size();
-            while (size--) {
-                auto [x, y] = q.front();
+        int dis=-1;
+        while(!q.empty()){
+            int s=q.size();
+            while(s--){
+                pair<int,int>cur=q.front();
                 q.pop();
-                for (auto [dx, dy] : dirs) {
-                    int i = x + dx, j = y + dy;
-                    if (i >= 0 && i < n && j >= 0 && j < n && visited[i][j] == 0) {
-                        visited[i][j] = 1;
-                        q.push({i, j});
+                for(auto it:dir){
+                    int x=cur.first+it.first;
+                    int y=cur.second+it.second;
+                    if(x>=0 && y>=0 && x<grid.size() && y<grid.size() && vis[x][y]==0){
+                        vis[x][y]=1;
+                        q.push({x,y});
                     }
                 }
             }
-            distance++;
+            dis++;
         }
-        return distance;}
+        if(dis==0){
+            return -1;
+        }
+        return dis;
+    }
 };

88749b88daa632939b5c9d3f8527fb1139f2afc4
Karthikeyan <76770213+karthikeyan12068@users.noreply.github.com>
2023-06-06 07:49:32 +0530

    Added solution - LeetHub

--- Replace O's with X's - GFG/replace-os-with-xs.cpp
+++ Replace O's with X's - GFG/replace-os-with-xs.cpp
@@ -9,49 +9,41 @@ using namespace std;
 
 class Solution{
 public:
-    bool dfs(int i,int j,vector<vector<char>> &mat){
-        if(i<0 || j<0 || i>=mat.size() || j>=mat[0].size()){
-            return false;
-        }
-        if(mat[i][j]=='X' || mat[i][j]=='$'){
-            return true;
-        }
-        mat[i][j]='$';
-        bool pos1=dfs(i+1,j,mat);
-        bool pos2=dfs(i,j+1,mat);
-        bool pos3=dfs(i-1,j,mat);
-        bool pos4=dfs(i,j-1,mat);
-        bool ans=false;
-        if(pos1==true && pos2==true && pos3==true && pos4==true){
-            ans=true;
-        }
-        if(ans==true){
-            mat[i][j]='P';
-        }
-        else{
-            mat[i][j]='*';
-        }
-        return ans;
-    }
+    
     vector<vector<char>> fill(int n, int m, vector<vector<char>> grid)
     {
-        for(int i=1;i<grid.size()-1;i++){
-            for(int j=1;j<grid[0].size()-1;j++){
-                if(grid[i][j]=='O' || grid[i][j]=='P'){
-                    bool ans=dfs(i,j,grid);
-                    if(ans==true){
-                        grid[i][j]='X';
-                    }
-                    else{
-                        grid[i][j]='*';
+        queue<pair<int,int>>q;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]=='O' && (i==0 || j==0 ||i==grid.size()-1 || j==grid[0].size()-1)){
+                    q.push({i,j});
+                }
+            }
+        }
+        vector<pair<int,int>>pos={{-1,0},{0,1},{1,0},{0,-1}};
+        while(!q.empty()){
+            int x=q.size();
+            while(x--){
+                auto node=q.front();
+                q.pop();
+                grid[node.first][node.second]='*';
+                for(auto it:pos){
+                    int x1=node.first+it.first;
+                    int y1=node.second+it.second;
+                    if(x1>=1 && y1>=1 && x1<grid.size()-1 && y1<grid[0].size()-1 && grid[x1][y1]=='O'){
+                        q.push({x1,y1});
+                        grid[x1][y1]='*';
                     }
                 }
             }
         }
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
-                if(grid[i][j]=='*' || grid[i][j]=='P'){
+                if(grid[i][j]=='*'){
                     grid[i][j]='O';
+                }
+                else{
+                    grid[i][j]='X';
                 }
             }
         }

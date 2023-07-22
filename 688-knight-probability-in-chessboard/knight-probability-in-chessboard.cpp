37f33ec055441f193fed1bf8395ebc1fd13d22b4
Karthikeyan <76770213+karthikeyan12068@users.noreply.github.com>
2023-07-23 00:42:47 +0530

    Time: 4 ms (96.87%) | Memory: 6.5 MB (89.79%) - LeetSync

--- 688-knight-probability-in-chessboard/knight-probability-in-chessboard.cpp
+++ 688-knight-probability-in-chessboard/knight-probability-in-chessboard.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
-    unordered_map<string,double> mp;
-    int dir[8][8] = {{-2,-1},{-1,-2},{1,-2},{2,-1},{2,1},{1,2},{-1,2},{-2,1}};
-
-    double knightProbability(int n, int k, int row, int column) {
-        return find(n,k,row,column);
+    
+    double memo[26][26][101];
+    int dxy[8][2]={{-2,1},{-1,2},{1,2},{2,1},{2,-1},{1,-2},{-1,-2},{-2,-1}};
+    
+    double prob(int n, int k, int r, int c){
+        if(memo[r][c][k]!=0) return memo[r][c][k];
+        
+        if(k==0) return 1; 
+        
+        double ans=0;
+        for(int d=0;d<8;d++){
+            int x=r+dxy[d][0], y=c+dxy[d][1];
+            
+            if(x>=0 && y>=0 && x<n && y<n) ans+=0.125*prob(n,k-1,x,y);
+        }
+        return memo[r][c][k]=ans;
     }
-
-    double find(int n, int moves, int r, int c) {
-        if(r<0 || r>=n || c<0 || c>=n)
-            return 0;
-        
-        if(moves == 0)
-            return 1;
-        
-        string key = to_string(r) + "aryan" + to_string(c) + "mittal" + to_string(moves);
-        if(mp.find(key) != mp.end())
-            return mp[key];
-        
-        double probability = 0;
-        for(int i=0; i<8; i++)
-            probability += find(n,moves-1, r+dir[i][0], c+dir[i][1])/8.0 ;
-        
-        mp[key] = probability;
-        return mp[key];
+    
+    double knightProbability(int n, int k, int r, int c) {
+       return prob(n,k,r,c);
     }
 };

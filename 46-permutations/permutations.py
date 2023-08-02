1bccf9870ef81c8be76a3661e3c8dacca508f155
Karthikeyan <76770213+karthikeyan12068@users.noreply.github.com>
2023-08-02 12:21:25 +0530

    Time: 49 ms (83.55%) | Memory: 16.5 MB (72.51%) - LeetSync

---
+++ 46-permutations/permutations.py
@@ -0,0 +1,39 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+
+        @cache
+        def fact(n):
+            if n<=0:
+                return 1
+            return n*fact(n-1)
+
+        val=fact(len(nums))
+        ans=[]
+        ans.append(list(nums))
+
+        def ret():
+            #function that gives the next smallest permuatation for current array
+            for j in range(len(nums)-2,-1,-1):
+                ind=-1
+                val1=float("inf")
+                for k in range(j+1,len(nums),1):
+                    if(val1>nums[k] and nums[k]>nums[j]):
+                        ind=k
+                        val1=nums[k]
+                if ind!=-1:
+                    nums[j],nums[ind]=nums[ind],nums[j]
+                    temp=[]
+                    for k in range(j+1,len(nums),1):
+                        temp.append(nums[k])
+                    temp.sort()
+                    x=0
+                    for k in range(j+1,len(nums),1):
+                        nums[k]=temp[x]
+                        x+=1
+                    break
+        for i in range(val-1):
+            #for every factorial iter we update array once
+            ret()
+            ans.append(list(nums))
+        return ans

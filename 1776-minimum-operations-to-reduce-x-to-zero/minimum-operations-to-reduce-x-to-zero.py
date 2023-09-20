6ff3a0e9383508c3155e179bc0b19da227c1e1b3
Karthikeyan <76770213+karthikeyan12068@users.noreply.github.com>
2023-09-20 18:40:11 +0530

    Time: 1016 ms (41.40%) | Memory: 30.9 MB (32.53%) - LeetSync

---
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minOperations(self, nums: List[int], x: int) -> int:
+        remsum=sum(nums)-x
+        if(remsum<0):
+            return -1
+        l=0
+        r=0
+        cursum=0
+        ans=-1
+        while(r<len(nums)):
+            cursum+=nums[r]
+            if(cursum==remsum):
+                if(ans==-1):
+                    ans=l+((len(nums)-r)-1)
+                else:
+                    ans=min(ans,l+((len(nums)-r)-1))
+            if(cursum>remsum):
+                while(l<=r and cursum>=remsum):
+                    cursum-=nums[l]
+                    l+=1
+                    if(cursum==remsum):
+                        if(ans==-1):
+                            ans=l+((len(nums)-r)-1)
+                        else:
+                            ans=min(ans,l+((len(nums)-r)-1))
+            r+=1
+
+        return ans
+

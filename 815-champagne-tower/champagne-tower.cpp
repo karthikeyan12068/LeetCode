fa78c0fcd73ab11a227ae4824c51c10cdbe3d9b9
Karthikeyan <76770213+karthikeyan12068@users.noreply.github.com>
2023-09-24 17:44:24 +0530

    Time: 989 ms (5.12%) | Memory: 70.1 MB (5.53%) - LeetSync

---
+++ 815-champagne-tower/champagne-tower.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    double champagneTower(int poured, int query_row, int query_glass) {
+        vector<vector<long double>>dp(100,vector<long double>(100,0.0));
+        long double x=(long double)poured;
+        //dp[0][0]=(double)poured;
+        dp[0][0]=x;
+        cout << std::fixed << dp[0][0] << std::endl;
+        for(int i=0;i<100;i++){
+            for(int j=0;j<=i;j++){
+                long double extra=dp[i][j]-1.0;
+                
+                if(extra>0.0){
+                    
+                    if(i+1<100){
+                        
+                        dp[i+1][j]+=(extra*0.5);
+                        dp[i+1][j+1]+=(extra*0.5);
+                    }
+                }
+                cout<<dp[i][j]<<' ';
+            }
+            cout<<'\n';
+        }
+        if(dp[query_row][query_glass]!=0.0){
+            if(dp[query_row][query_glass]<1.0){
+                return dp[query_row][query_glass];
+            }
+            return (double)1.0;
+        }
+        return (double)0.0;
+        
+    }
+};

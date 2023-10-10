8629cd27714f5bf681fa90e34df3c9b762b5d4b8
Karthikeyan <76770213+karthikeyan12068@users.noreply.github.com>
2023-10-10 18:31:32 +0530

    Time: 406 ms (11.51%) | Memory: 104.7 MB (35.15%) - LeetSync

---
+++ 2119-minimum-number-of-operations-to-make-array-continuous/minimum-number-of-operations-to-make-array-continuous.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+ 
+    int minOperations(vector<int>& nums) {
+        set<int>s;
+        int n=nums.size();
+        for(auto it:nums){
+            s.insert(it);
+        }
+        nums.clear();
+        for(auto it:s){
+            nums.push_back(it);
+        }
+        int val=INT_MAX;
+        for(int i=0;i<nums.size();i++){
+            auto it=upper_bound(nums.begin(),nums.end(),nums[i]+(n-1));
+            int ind=it-nums.begin();
+            int dis=ind-i;
+            val=min(val,n-dis);
+        }
+        return val;
+    }
+};

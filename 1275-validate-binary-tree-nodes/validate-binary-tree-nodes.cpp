e8241e1b53b73e7c425cba261df6ab145443583f
Karthikeyan <76770213+karthikeyan12068@users.noreply.github.com>
2023-10-17 23:19:37 +0530

    Time: 58 ms (40.12%) | Memory: 39.7 MB (40.88%) - LeetSync

---
+++ 1275-validate-binary-tree-nodes/validate-binary-tree-nodes.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    bool validateBinaryTreeNodes(int n, vector<int>& leftChild, vector<int>& rightChild) {
+        vector<int>vis(n,-1);
+        for(int i=0;i<n;i++){
+            if(vis[i]!=-1){
+                continue;
+            }
+            vis[i]=2;
+            queue<int>q;
+            q.push(i);
+            while(!q.empty()){
+                int x=q.size();
+                while(x--){
+                    int node=q.front();
+                    
+                    q.pop();
+                    if(leftChild[node]!=-1){
+                        if(vis[leftChild[node]]==-1){
+                            vis[leftChild[node]]=1;
+                            q.push(leftChild[node]);
+                        }
+                        else{
+                            if(vis[leftChild[node]]==2 && vis[node]==2){
+                                vis[leftChild[node]]=1;
+                                
+                            }
+                            else{
+                                return false;
+                            }
+                        }
+                    }
+                    if(rightChild[node]!=-1){
+                        if(vis[rightChild[node]]==-1){
+                            vis[rightChild[node]]=1;
+                            q.push(rightChild[node]);
+                        }
+                        else{
+                            if(vis[rightChild[node]]==2 && vis[node]==2){
+                                vis[rightChild[node]]=1;
+                                
+                            }
+                            else{
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        int c=0;
+        for(auto it:vis){
+            cout<<it<<' ';
+            if(it==2){
+                c++;
+            }
+        }
+        if(c>1){
+            return false;
+        }
+        return true;
+    }
+};

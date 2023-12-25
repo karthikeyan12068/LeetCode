1659d7deb7fc188fc9d502c359e270296fb14d6f
Karthikeyan <76770213+karthikeyan12068@users.noreply.github.com>
2023-12-25 22:33:24 +0530

    Time: 347 ms (11.11%) | Memory: 91.5 MB (11.11%) - LeetSync

--- 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.cpp
+++ 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
     long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
-        vector<vector<long long>>matrix(26,vector<long long>(26,INT_MAX));
+        vector<vector<int>>matrix(26,vector<int>(26,1e9));
         for(int i=0;i<original.size();i++){
-            int x=original[i]-'a',y=changed[i]-'a';
-            matrix[x][y]=min(matrix[x][y],(long long)cost[i]);
+            matrix[original[i]-'a'][changed[i]-'a']=min(matrix[original[i]-'a'][changed[i]-'a'],cost[i]);
         }
         int n=26;
-        
+        for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (matrix[i][j] == -1) {
+					matrix[i][j] = 1e9;
+				}
+				if (i == j) matrix[i][j] = 0;
+			}
+		}
 
-		for (int k = 0; k < 26; k++) {
-			for (int i = 0; i < 26; i++) {
-				for (int j = 0; j < 26; j++) {
-                    long long v=matrix[i][k] + matrix[k][j];
-                    matrix[i][j] = min(matrix[i][j],v);
-                    
+		for (int k = 0; k < n; k++) {
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					matrix[i][j] = min(matrix[i][j],matrix[i][k] + matrix[k][j]);
 				}
 			}
 		}
         long long ans=0;
         for(int i=0;i<source.length();i++){
             if(source[i]!=target[i]){
-                if(matrix[source[i]-'a'][target[i]-'a']==LONG_MAX){
+                if(matrix[source[i]-'a'][target[i]-'a']==1e9){
                     return -1;
                 }
-                int x=source[i]-'a',y=target[i]-'a';
-                if(matrix[x][y]>=INT_MAX){
-                    return -1;
-                }
-                ans+=matrix[x][y];
+                ans+=matrix[source[i]-'a'][target[i]-'a'];
             }
         }
         return ans;

8da371ee462d3c685a2f2440448d8a80826eb1ef
Karthikeyan <76770213+karthikeyan12068@users.noreply.github.com>
2024-01-06 12:59:45 +0530

    Time: 118 ms (90.01%) | Memory: 83 MB (31.98%) - LeetSync

---
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int dp[50001];
+    int ret(vector<pair<pair<int,int>,int>>&v,int i){
+        if(i==v.size()){
+            return 0;
+        }
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        int take=v[i].second,ntake=0;
+        std::pair<std::pair<int, int>, int> value_to_find = { {v[i].first.second, 0}, 0 }; 
+        auto it = std::lower_bound(
+        v.begin(),
+        v.end(),
+        value_to_find,
+        [](const pair<pair<int, int>, int>& a, const std::pair<std::pair<int, int>, int>& b) {
+            return a.first < b.first; 
+        }
+    );
+        if(it!=v.end()){
+            int ind=it-v.begin();
+            take+=ret(v,ind);
+        }
+        ntake+=ret(v,i+1);
+        return dp[i]=max(take,ntake);
+    }
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        memset(dp,-1,sizeof(dp));
+        vector<pair<pair<int,int>,int>>v;
+        for(int i=0;i<startTime.size();i++){
+            v.push_back({{startTime[i],endTime[i]},profit[i]});
+        }
+        sort(v.begin(), v.end(),
+              [](const std::pair<std::pair<int, int>, int>& a, const std::pair<std::pair<int, int>, int>& b) {
+                  return a.first < b.first; // Comparing based on the first element of the pairs within the pairs
+              });        
+        return ret(v,0);
+    }
+};

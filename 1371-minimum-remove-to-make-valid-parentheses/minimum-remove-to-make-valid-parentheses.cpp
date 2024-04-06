c183d3eccd7904494a2e6ff783dfa2143d059b36
Karthikeyan <76770213+karthikeyan12068@users.noreply.github.com>
2024-04-06 17:24:34 +0530

    Time: 91 ms (5.01%) | Memory: 44.3 MB (5.01%) - LeetSync

---
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        stack<int>st;
+        unordered_map<int,int>m;
+        for(int i=0;i<s.length();i++){
+            if(s[i]==')' || s[i]=='('){
+                if(s[i]==')'){
+                    if(st.empty()){
+                        m[i]++;
+                    }else{
+                        st.pop();
+                    }
+                }else{
+                    st.push(i);
+                }
+            }
+        }
+        while(!st.empty()){
+            m[st.top()]++;
+            st.pop();
+        }
+        string ans="";
+        for(int i=0;i<s.length();i++){
+            if(m[i]==0){
+                ans+=s[i];
+            }
+        }
+        return ans;
+    }
+};

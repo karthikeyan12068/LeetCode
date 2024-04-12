64c7978f50846d1a578f1e23e0cc074bdc40bf0c
Karthikeyan <76770213+karthikeyan12068@users.noreply.github.com>
2024-04-12 23:34:56 +0530

    Time: 11 ms (54.11%) | Memory: 23.3 MB (7.88%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,31 +1,40 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        vector<int>pref(height.size()),suf(height.size());
+        vector<int>l(height.size()),r(height.size());
         int prev=-1;
-        for(int i=0;i<height.size();i++){
-            if(height[i]>prev){
-                prev=height[i];
-                pref[i]=height[i];
-            }
-            else{
-                pref[i]=prev;
+        for(int i=height.size()-1;i>=0;i--){
+            if(prev==-1){
+                l[i]=height[i];
+                prev=i;
+            }else{
+                if(height[prev]<=height[i]){
+                    prev=i;
+                    l[i]=height[i];
+                }else{
+                    l[i]=height[prev];
+                }
             }
         }
         prev=-1;
-        for(int i=height.size()-1;i>=0;i--){
-            if(height[i]>prev){
-                prev=height[i];
-                suf[i]=height[i];
-            }
-            else{
-                suf[i]=prev;
-            }
-        }
-        int sum=0;
         for(int i=0;i<height.size();i++){
-            sum+=(min(pref[i],suf[i])-height[i]);
+            if(prev==-1){
+                r[i]=height[i];
+                prev=i;
+            }else{
+                if(height[prev]<=height[i]){
+                    prev=i;
+                    r[i]=height[i];
+                }else{
+                    r[i]=height[prev];
+                }
+            }
         }
-        return sum;
+        int ans=0;
+        for(int i=0;i<height.size();i++){
+            ans+=abs(height[i]-min(l[i],r[i]));
+        }
+        
+        return ans;
     }
 };
